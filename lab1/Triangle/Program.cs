b4c6384ea7b3bbfd07c373d8d66a798b3416095a
GreatHorizon <52240672+GreatHorizon@users.noreply.github.com>
2020-09-14 17:07:50 +0300

    Added lab1

---
+++ lab1/Triangle/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Invalid argument count. It should be: triangle.exe <side1> <side2> <side3>");
+                return;
+            }
+            try
+            {
+                double[] sides = CheckArguments(args);
+                Console.WriteLine(GetTriangleType(sides));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static string GetTriangleType(double[] sides)
+        {
+            double a = sides[0];
+            double b = sides[1];
+            double c = sides[2];
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+            {
+                return "Triangle does not exist";
+            }
+
+            else if (a == b && b == c)
+            {
+                return "Equilateral";
+            }
+
+            else if (a == b || a == c || b == c)
+            {
+                return "Isosceles";
+            }
+
+            return "Usual";
+        }
+
+        public static double[] CheckArguments(string[] args)
+        {
+            double[] result = new double[3];
+
+            try
+            {
+                result[0] = Double.Parse(args[0], CultureInfo.InvariantCulture);
+                result[1] = Double.Parse(args[1], CultureInfo.InvariantCulture);
+                result[2] = Double.Parse(args[2], CultureInfo.InvariantCulture);
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("Sides should be numeric");
+            }
+
+
+            if (result[0] == 0 || result[1] == 0 || result[2] == 0)
+            {
+                throw new ArgumentException("Triangle sides should not be equal to 0");
+            }
+
+            return result;
+        }
+    }
+}
---
+++ lab1/TriangleTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using Triangle;
+
+namespace TriangleTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            int testCounter = 1;
+            using var outputFile = new StreamWriter("../../../output.txt", false);
+
+            using (var inputFile = new StreamReader("../../../input.txt"))
+            {
+                string testCaseArguments;
+                string expectedOutput;
+                string receivedOutput;
+
+                while ((testCaseArguments = inputFile.ReadLine()) != null)
+                {
+                    string[] args = testCaseArguments.Split(' ');
+                    expectedOutput = inputFile.ReadLine();
+
+                    var output = new StringWriter();
+                    Console.SetOut(output);
+                    Console.SetError(output);
+
+                    Triangle.Program.Main(args);
+                    receivedOutput = output.ToString();
+                    receivedOutput = receivedOutput.Replace("\n", "").Replace("\r", "");
+
+                    if (receivedOutput == expectedOutput)
+                    {
+                        outputFile.WriteLine("Test passed " + testCounter + ": successfully");
+                    }
+                    else
+                    {
+                        outputFile.WriteLine("Test " + testCounter + ": failed. Expected: " + expectedOutput + " Recieved: " + receivedOutput);
+                    }
+
+                    testCounter++;
+                }
+            }
+        }
+    }
+}
